e76c28b6f8100294f6b35080fc2af85a645364f0
Robbie <yashoct568@gmail.com>
2024-05-18 13:14:44 +0530

    main.c

--- main.c
+++ main.c
@@ -4,6 +4,6 @@
 
 int main() {
     printf("Prime Test Case Score: %.2f\n", testcase_prime());
-    printf("Power Test Case Score: %d\n", testcase_power());
+    printf("Power Test Case Score: %0.2f\n", testcase_power());
     return 0;
 }
