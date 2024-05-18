5b8d61e730916390899d4b2a7bbdcde9f28713a9
gamdev <princep23@iitk.ac.in>
2024-05-19 01:11:43 +0530

    changed %f instead of %d

--- run.c
+++ run.c
@@ -5,8 +5,8 @@ int main(){
     float r2=testcase_fibonacci();
     float r3=testcase_power();
     float r4=testcase_factorial();
-    printf("accuracy in testing prime is %d%%",r1);
-    printf("accuracy in testing fibonacci is %d%%",r2);
-    printf("accuracy in testing power is %d%%",r3);
-    printf("accuracy in testing factorial is %d%%",r4);
+    printf("accuracy in testing prime is %f%%\n",r1);
+    printf("accuracy in testing fibonacci is %f%%\n",r2);
+    printf("accuracy in testing power is %f%%\n",r3);
+    printf("accuracy in testing factorial is %f%%\n",r4);
 }
