1b4cb86810efd09a345a00c106813b452b590119
Robbie <yashoct568@gmail.com>
2024-05-18 13:06:28 +0530

    updating function

--- function.h
+++ function.h
@@ -1,60 +1,31 @@
-#include<stdio.h>
-#include<math.h>
-#include<ctype.h>
-int factorial(int n){
-    if(isdigit(n)){
-    if(n<0) return -1;
-    if(n==0) return 1;
-    else return n*factorial(n-1);
-}
-else return -1
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+int factorial(int n) {
+    if (n < 0) return -1;
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
 }
 
-int fibonacci(int n){
-if(isdigit(n)){
+int fibonacci(int n) {
+    if (n < 0) return -1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
-    if (n == 0)
-    {
-        return 0;
+int power(int i, int j) {
+    if (j == 0) return 1;
+    if (j < 0) return -1; 
+    return i * power(i, j - 1);
+}
+
+int isPrime(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) return 0;
     }
-    else if (n == 1)
-    {
-        return 1;
-    }
-    else if (n > 1)
-    {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-    else if (n < 0)
-    {
-        return -1;
-    }
-
-}
-else return -1;
-}
-int power(int i, int j){
-    if(isdigit(i)==1&&isdigit(j)==1){
-    if(j==1) return i;
-    else if(j==0) return 1;
-    else if (j<0) return -1;
-    else return power(i,j-1);
-}
-else return -1
+    return 1;
 }
 
-int isPrime(int n){
-  if(isdigit(n)){
-  if(n<0) return -1;
-  else{
-  for(int i = 2; i < n/2; i++){
-    if(n%i==0) return 0;
-    else continue; 
-  }
-  return 1;
-}
-}
-else return -1;
-}
-
-
+#endif
